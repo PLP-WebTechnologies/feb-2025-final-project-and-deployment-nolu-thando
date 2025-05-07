58ea21789401860f8efcc4e4dcae559eb4652908
Noluthando Mphande <noluthandowendy2013@gmail.com>
2025-05-07 15:46:13 +0200

    files updated

--- Project folder/js/script.js
+++ Project folder/js/script.js
@@ -1,4 +1,4 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     alert("Welcome to My Blog!");
-//   });
+document.addEventListener('DOMContentLoaded', () => {
+    alert("Welcome to My Blog!");
+  });
   
